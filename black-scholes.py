9bffcc3e2a217f2888617adb92a46570c49f0fc2
Eric Jansen <personal@Erics-MacBook-Air.local>
2025-05-02 14:57:22 +0700

    Added title

---
+++ black-scholes.py
@@ -0,0 +1,14 @@
+from helper import black_scholes_call, black_scholes_put, create_range
+import math
+import matplotlib.pyplot as plt
+import pandas as pd
+from scipy.stats import norm
+import seaborn as sns
+import streamlit as st
+
+sns.set_context("notebook", font_scale=0.8)
+
+# Request user input
+st.title("Black-Scholes Options Pricing and PnL")
+
+# Generate heatmap based on input
